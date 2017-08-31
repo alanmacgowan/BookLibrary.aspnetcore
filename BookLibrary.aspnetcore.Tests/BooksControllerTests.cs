2c0710af53a20d1a76388a3166acd762acf3b9b8
alanmacgowan <alanmacgowan@gmail.com>
2017-08-31 20:29:49 -0300

    refactored tests

--- BookLibrary.aspnetcore.Tests/BooksControllerTests.cs
+++ BookLibrary.aspnetcore.Tests/BooksControllerTests.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using BookLibrary.aspnetcore.Controllers;
 using BookLibrary.aspnetcore.Domain;
 using BookLibrary.aspnetcore.Services;
@@ -15,19 +16,23 @@ namespace BookLibrary.aspnetcore.Tests
 {
     public class BooksControllerTests
     {
+        IMapper mapper;
+
+        public BooksControllerTests()
+        {
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfileConfiguration());
+            });
+             mapper = config.CreateMapper();
+        }
+
         [Fact]
         public async Task Index_ReturnsAViewResult_WithAListOfBooks()
         {
             // Arrange
             var mockService = new Mock<IBookService>();
-            mockService.Setup(srvc => srvc.GetAll()).Returns(Task.FromResult(GetTestBookList()));
-
-            //move to other method
-            var config = new AutoMapper.MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile(new AutoMapperProfileConfiguration());
-            });
-            var mapper = config.CreateMapper();
+            mockService.Setup(srvc => srvc.GetAll()).Returns(Task.FromResult(TestHelper.GetTestBookList()));
 
             var controller = new BooksController(mapper, mockService.Object);
 
@@ -39,27 +44,6 @@ namespace BookLibrary.aspnetcore.Tests
             var model = Assert.IsAssignableFrom<IEnumerable<BookViewModel>>(viewResult.ViewData.Model);
             Assert.Equal(1, model.Count());
         }
-
-        private IEnumerable<Book> GetTestBookList()
-        {
-            var books = new List<Book>();
-
-            books.Add(new Book {
-                Title = "Book 1",
-                Description = "This is Book number 1",
-                PublishDate = DateTime.Parse("2005-09-01"),
-                ISBN = "1289189218291",
-                Category = "Comedy",
-                Pages = 111,
-                Language = "Spanish",
-                PublisherID = 1,
-                AuthorID = 1 });
-
-            return  books;
-        }
-
-
-
 
 
     }
---
+++ BookLibrary.aspnetcore.Tests/TestHelper.cs
@@ -0,0 +1,31 @@
+﻿using BookLibrary.aspnetcore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookLibrary.aspnetcore.Tests
+{
+    public static class TestHelper
+    {
+        public static IEnumerable<Book> GetTestBookList()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book
+            {
+                Title = "Book 1",
+                Description = "This is Book number 1",
+                PublishDate = DateTime.Parse("2005-09-01"),
+                ISBN = "1289189218291",
+                Category = "Comedy",
+                Pages = 111,
+                Language = "Spanish",
+                PublisherID = 1,
+                AuthorID = 1
+            });
+
+            return books;
+        }
+
+    }
+}
