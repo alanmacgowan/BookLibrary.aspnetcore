3931ce42c1cea58d3974fde181cb61dcf1d1df53
alanmacgowan <alanmacgowan@gmail.com>
2017-09-03 19:06:10 -0300

    added gulp

---
+++ BookLibrary.aspnetcore.UI/gulpfile.js
@@ -0,0 +1,71 @@
+﻿/// <binding AfterBuild='copy:libs:dev' />
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    del = require('del'),
+    sequence = require('run-sequence'),
+    jsPath = 'wwwroot/js',
+    jsDist = 'wwwroot/js/dist',
+    cssPath = 'wwwroot/css',
+    cssDist = 'wwwroot/css/dist',
+    libPath = 'wwwroot/lib',
+    nodeModulesPath = 'node_modules';
+
+gulp.task('clean', function () {
+    return del(jsDist, { force: true });
+});
+
+gulp.task('copy:libs:dev', function (done) {
+    sequence('clean', 'copy:vendor:dev', 'copy:vendor:css', done);
+});
+
+gulp.task('copy:libs:prod', function (done) {
+    sequence('clean', 'copy:vendor:prod', 'copy:vendor:css', done);
+});
+
+gulp.task('copy:vendor:css', function () {
+    return gulp.src([
+        libPath + '/bootstrap/dist/css/bootstrap.min.css',
+        libPath + '/components-font-awesome/css/font-awesome.css',
+        libPath + '/datatables.net-dt/css/jquery.dataTables.css',
+        libPath + '/bootstrap3-dialog/dist/css/bootstrap-dialog.css'
+    ])
+        .pipe(concat('scripts.vendor.css'))
+        .pipe(gulp.dest(cssDist));
+});
+
+gulp.task('copy:vendor:dev', function () {
+    return gulp.src([
+        libPath + '/jquery/dist/jquery.js',
+        libPath + '/bootstrap/dist/js/bootstrap.js',
+        libPath + '/datatables.net/js/jquery.dataTables.js',
+        libPath + '/bootstrap3-dialog/dist/js/bootstrap-dialog.js',
+        nodeModulesPath + '/moment/moment.js'
+    ])
+        .pipe(concat('scripts.vendor.js'))
+        .pipe(gulp.dest(jsDist));
+});
+
+gulp.task('copy:vendor:prod', function () {
+    return gulp.src([
+        libPath + '/jquery/dist/jquery.js',
+        libPath + '/bootstrap/dist/js/bootstrap.js',
+        libPath + '/datatables.net/js/jquery.dataTables.js',
+        libPath + '/bootstrap3-dialog/dist/js/bootstrap-dialog.js',
+        nodeModulesPath + '/moment/moment.js'
+    ])
+        .pipe(concat('scripts.vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDist));
+});
+
+gulp.task('watch', function () {
+
+    gulp.watch([
+        jsPath + '/**/*.js', ['compressScripts']
+    ]);
+
+});
+
+gulp.task('default', ['compressScripts', 'watch']);
+
