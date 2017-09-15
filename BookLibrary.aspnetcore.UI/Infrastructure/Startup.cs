18127688ba2799cb2248959de8636594041e80b1
alanmacgowan <alanmacgowan@gmail.com>
2017-09-15 20:41:47 -0300

    added logging

--- BookLibrary.aspnetcore.UI/Infrastructure/Startup.cs
+++ BookLibrary.aspnetcore.UI/Infrastructure/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace BookLibrary.aspnetcore.UI.Infrastructure
 {
@@ -34,8 +35,11 @@ namespace BookLibrary.aspnetcore.UI.Infrastructure
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
