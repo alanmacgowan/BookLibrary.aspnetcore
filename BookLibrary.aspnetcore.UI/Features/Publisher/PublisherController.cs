7c5e78f27b5b9360792f9e3697a25a0ac6e78f24
Alan Macgowan <alanmacgowan@hotmail.com>
2017-09-23 18:33:00 -0600

    publishers module initial commit

---
+++ BookLibrary.aspnetcore.UI/Features/Publisher/PublisherController.cs
@@ -0,0 +1,35 @@
+﻿namespace BookLibrary.aspnetcore.UI.Features.Publisher
+{
+    using AutoMapper;
+    using BookLibrary.aspnetcore.Domain;
+    using BookLibrary.aspnetcore.Services.Interfaces;
+    using BookLibrary.aspnetcore.UI.Infrastructure;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class PublisherController : BaseController
+    {
+
+        private IPublisherService _publisherService;
+
+        public PublisherController(IMapper mapper, IPublisherService publisherService) : base(mapper)
+        {
+            _publisherService = publisherService;
+        }
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IEnumerable<PublisherViewModel>> GetPublishers()
+        {
+            var publishers = await _publisherService.GetAll();
+            return _mapper.Map<IEnumerable<Publisher>, IEnumerable<PublisherViewModel>>(publishers);
+        }
+        
+    }
+
+}
